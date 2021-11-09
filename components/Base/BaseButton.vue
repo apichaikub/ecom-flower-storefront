f19dd082f9087d802e90dcca94e6f1bf16054393
_CHAI__ <apichai.freedom@gmail.com>
2021-11-09 12:38:28 +0700

    Revert "c1"
    This reverts commit a73419027949674135d02ef7ce88fb052f2116f2.

--- components/Base/BaseButton.vue
+++ components/Base/BaseButton.vue
@@ -3,7 +3,6 @@
         <slot>{{ text }}</slot>
         
         
-
     </button>
 </template>
 
@@ -11,7 +10,6 @@
 export default {
     props: {
         text: String,
-        
         
         
     }
